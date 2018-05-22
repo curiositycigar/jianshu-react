c5a70f2183827ab71ad7b584ba10a7f9acc38eb2
curiositycigar <you11098@163.com>
2018-05-22 17:30:53 +0800

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import * as React from "react";
 import {Button} from "antd";
 import {
+  HashRouter,
+} from 'react-router-dom'
+import {
   Link,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom'
-import Home from './containers'
+import Header from './containers/Header'
+import Home from './containers/Index'
 import Home1 from './containers/Home'
 import NotFound from './components/NotFound'
+import MainContainer from './components/MainContainer'
 import * as logo from "./logo.svg";
 import "./App.css";
 
@@ -22,31 +27,38 @@ class App extends React.Component<any, any> {
   public render () {
     console.log(this.props.children)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <Link to="/">
-            <Button type="primary">Root</Button>
-          </Link>
-          <Link to="/home">
-            <Button type="primary">Home</Button>
-          </Link>
-        </header>
-        <section>
-        <p className="App-intro">
-          To get started, edit <code>s1rc/App.tsx</code> and save to reload.
-        </p>
-        <Switch>
-          <Route path="/index" component={Home}/>
-          <Route path="/home" component={Home1}/>
-          <Redirect from="/" to="/index"/>
-          <Route path="/*" component={NotFound}/>
-        </Switch>
-        </section>
-        <footer>
-          this is footer
-        </footer>
-      </div>
+      <HashRouter>
+        <div className="App">
+          <Header/>
+          <div style={{display: 'none'}} className="App-header">
+            <nav>
+              <a href="/">繁体</a>
+            </nav>
+            <img src={logo} className="App-logo" alt="logo"/>
+            <Link to="/">
+              <Button type="primary">Root</Button>
+            </Link>
+            <Link to="/home">
+              <Button type="primary">Home</Button>
+            </Link>
+          </div>
+          <section style={{display: 'none'}}>
+            <MainContainer>Layout</MainContainer>
+            <p className="App-intro">
+              To get started, edit <code>s1rc/App.tsx</code> and save to reload.
+            </p>
+            <Switch>
+              <Route path="/index" component={Home}/>
+              <Route path="/home" component={Home1}/>
+              <Redirect from="/" to="/index"/>
+              <Route path="/*" component={NotFound}/>
+            </Switch>
+          </section>
+          <footer>
+            this is footer
+          </footer>
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/containers/SignIn.tsx
@@ -0,0 +1,21 @@
+/**
+ * Created by YOU on 2018/5/22.
+ */
+import * as React from 'react'
+// import propTypes = require('prop-types')
+// import classNames = require('classnames')
+// import {omit} from 'lodash'
+
+class SignIn extends React.Component<any, any> {
+  constructor (props: any) {
+    super(props)
+  }
+
+  public render () {
+    return (
+      <div/>
+    )
+  }
+}
+
+export default SignIn
