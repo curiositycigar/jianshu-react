e570e4919b0614b1f8ae6c14028c621bf403bab1
curiositycigar <you11098@163.com>
2018-05-24 23:14:54 +0800

    commit

---
+++ src/components/Button..tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+class Button extends React.Component {
+    constructor(props: any) {
+        super(props)
+    }
+
+    public render() {
+        return <div/>
+    }
+}
+
+export default Button
