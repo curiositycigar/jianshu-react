1708b2f51a459b20d9b07a149b07de1be317c8fd
curiositycigar <you11098@163.com>
2018-06-06 17:52:44 +0800

    测试

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,26 +1,20 @@
 /**
  * Created by YOU on 2018/4/28.
  */
-import * as React from 'react'
-import {
-  HashRouter,
-} from 'react-router-dom'
-import App from '../App'
-// import propTypes = require('prop-types')
-// import classNames = require('classnames')
-// import {omit} from 'lodash'
-class RouterConfig extends React.Component<any, any> {
-  constructor (props: any) {
-    super(props)
-  }
-
-  public render () {
-    return (
-      <HashRouter>
-        <App/>
-      </HashRouter>
-    )
-  }
-}
-
-export default RouterConfig
+// interface IRoute {
+//   path: string,
+//   name?: string,
+//   component?: JSX.Element,
+//   children?: IRoute[]
+// }
+//
+// const routes: IRoute[] = [
+//   {
+//     path: '/',
+//     component: <div/>
+//   }
+// ]
+//
+// console.log(routes)
+//
+// export default <div/>
